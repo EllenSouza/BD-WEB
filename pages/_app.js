459d766d68a7d51e4f928317e22371a1f00b664c
Ellen Souza <ellen.almeida.souza21@gmail.com>
2022-12-19 00:21:32 -0300

    Adiciona primereact e chart.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,8 @@
+import 'primereact/resources/themes/lara-light-teal/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+import 'primeflex/primeflex.css'; //icons
+
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
