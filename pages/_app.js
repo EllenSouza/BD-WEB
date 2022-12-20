05df76e49548f5f47096ce953e4105a53230814f
keviinsna <keevinsenna2001@gmail.com>
2022-12-20 02:01:36 -0300

    Atualiza tela inicial

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import 'primereact/resources/themes/lara-light-teal/theme.css'; //theme
 import 'primereact/resources/primereact.min.css'; //core css
 import 'primeicons/primeicons.css'; //icons
-import 'primeflex/primeflex.css'; //icons
+import 'primeflex/primeflex.css'; //flex
+
+import '../styles/globals.css';
+import Layout from '../components/layout';
+
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
