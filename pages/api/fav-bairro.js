54e2d4200f5c8799dfd86fc3a578ad21fad19679
keviinsna <keevinsenna2001@gmail.com>
2022-12-20 17:36:07 -0300

    Adapta ao pool connection

--- pages/api/fav-bairro.js
+++ pages/api/fav-bairro.js
@@ -1,28 +1,24 @@
-// get the client
-const mysql = require('mysql2/promise');
+import { pool } from '../../utils/config';
+
 export default async function connection(req, res) {
-    // create the connection to database
-    const connection = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-    });
     try {
-        const [results] =
+        const connection = await pool.getConnection();
+
+        const [fav_bairro] =
             await connection.query(`SELECT Nome_Bairro, COUNT(Cod_Fav) as Qtd_Favelas
         FROM Favela NATURAL RIGHT JOIN Bairro
         GROUP BY Cod_Bairro
         ORDER BY Qtd_Favelas DESC
         `);
-        const [results2] =
+        const [fav_ap] =
             await connection.query(`SELECT Nome_AP, COUNT(cod_fav) as Qtd_Favelas
             FROM Bairro NATURAL JOIN Area_de_Planejamento NATURAL JOIN Favela
             GROUP BY Cod_AP;
         `);
+        pool.releaseConnection(connection);
         res.status(200).json({
-            Quantidade_de_favelas_por_bairro: results,
-            Quantidade_de_favelas_por_área_de_planejamento: results2,
+            Quantidade_de_favelas_por_bairro: fav_bairro,
+            Quantidade_de_favelas_por_área_de_planejamento: fav_ap,
         });
     } catch (error) {}
 }
