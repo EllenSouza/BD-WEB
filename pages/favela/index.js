66339c39e96e671824b2e3f3c13f2f33a5da5287
keviinsna <keevinsenna2001@gmail.com>
2022-12-20 14:39:58 -0300

    Adiciona tela para resultado de consultas sobre favelas Cria primeiro gráfico sobre quantidade de favelas por bairro

---
+++ pages/favela/index.js
@@ -0,0 +1,69 @@
+import { Chart } from 'primereact/chart';
+import { BDWebService } from '../../services/bd-web';
+import { useEffect, useState } from 'react';
+
+export default function favela() {
+    const service = new BDWebService();
+
+    const getQtdFavBairro = async () => {
+        const resp = await service.getQtdFavelasPorBairro();
+        return resp;
+    };
+    const [labels, setLabels] = useState([]);
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const getData = async () => {
+            const label = [];
+            const result = [];
+            const qtdFavPorBairro = await getQtdFavBairro();
+            qtdFavPorBairro.map((registro) => {
+                label.push(registro.Nome_Bairro);
+                result.push(registro.Qtd_Favelas);
+            });
+            setLabels(label);
+            setResults(result);
+        };
+        getData();
+        console.log(labels);
+        console.log(results);
+    }, []);
+
+    const data = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Quantidade de favelas',
+                data: results,
+                fill: false,
+                backgroundColor: '#4cd07c',
+            },
+        ],
+    };
+    const options = {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Quantidade de favelas por Bairro (RJ)',
+                font: {
+                    size: 16,
+                },
+            },
+            legend: {
+                position: 'bottom',
+            },
+        },
+    };
+    return (
+        <>
+            <div className='flex justify-content-center'>
+                <Chart
+                    type="bar"
+                    data={data}
+                    options={options}
+                    style={{ width: 1200 }}
+                />
+            </div>
+        </>
+    );
+}
