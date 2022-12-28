5998563115faf21389bb6f715f5cffc2a14a360b
keviinsna <keevinsenna2001@gmail.com>
2022-12-28 09:44:43 -0300

    Adiciona documentação aos métodos dos services

--- services/favela-service.js
+++ services/favela-service.js
@@ -1,15 +1,27 @@
 import { BASE_URL } from '../utils/config';
 export class FavelaService {
+    /**
+     * Busca todas as favelas da base
+     * @returns {Array<Object>} Array com todas as favelas
+     */
     async getFavelas() {
         const res = await (await fetch(BASE_URL + '/favela')).json();
         return res.data;
     }
 
+    /**
+     * Busca top 30 quantidade de favelas em cada bairro
+     * @returns {Array<Object>} Array com os bairros e a quantidade de favelas
+     */
     async getQtdFavelasPorBairro() {
         const res = await (await fetch(BASE_URL + '/favela/bairro')).json();
         return res.data;
     }
 
+    /**
+     * Busca a quantidade de favelas por área de planejamento
+     * @returns {Array<Object>} Array com as AP e a quantidade de favelas
+     */
     async getQtdFavelasPorAP() {
         const res = await (
             await fetch(BASE_URL + '/favela/area_planejamento')
@@ -17,16 +29,22 @@ export class FavelaService {
         return res.data;
     }
 
+    /**
+     * Busca a quantidade de favelas por índice de urbanização
+     * @returns {Array<Object>} Array com os índices de urbanização e a quantidade de favelas
+     */
     async getQtdFavelaPorUrbanizacao() {
         const res = await (
             await fetch(BASE_URL + '/favela/urbanizacao')
         ).json();
         return res.data;
     }
-    async getQtdFavelasPorPopulacao(){
-        const res = await (
-            await fetch(BASE_URL + '/favela/populacao')
-        ).json();
+    /**
+     * Busca a quantidade de favelas por faixa de população
+     * @returns {Array<Object>} Array com as faixas de população e a quantidade de favelas
+     */
+    async getQtdFavelasPorPopulacao() {
+        const res = await (await fetch(BASE_URL + '/favela/populacao')).json();
         return res.data;
     }
 }
