7b0d3b0c82f9bbb22d093cc9ad9b17e9cf8a9255
Ellen Souza <ellen.almeida.souza21@gmail.com>
2023-01-03 01:35:48 -0300

    Recupera dados de favela

--- pages/favela/[fid].jsx
+++ pages/favela/[fid].jsx
@@ -1,5 +1,23 @@
+import { FavelaService } from '../../services/favela-service';
+import { useEffect } from 'react';
 export default function Favela({ loading, query }) {
-    return <h1>olá</h1>;
+    const service = new FavelaService();
+    const getFavela = async () => await service.getFavela(query.fid);
+
+    useEffect(() => {
+        const initScreen = async () => {
+            try {
+                const getFavelaInfo = await getFavela();
+                console.log(getFavelaInfo);
+            } catch (error) {}
+        };
+        initScreen();
+    }, []);
+    return <>
+        <div>
+            
+        </div>
+    </>;
 }
 export async function getServerSideProps({ query }) {
     return { props: { query } };
