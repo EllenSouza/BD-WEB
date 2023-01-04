b477d65af2b01294281b7dbd67dce6f4e49e6aa1
Ellen Souza <ellen.almeida.souza21@gmail.com>
2023-01-04 00:34:10 -0300

    Corrige query de favela

--- pages/api/favela/[fid].js
+++ pages/api/favela/[fid].js
@@ -7,12 +7,12 @@ export default async function favelas(req, res) {
     try {
         const { fid } = req.query;
 
-        const favelas = await executeQuery(
+        const [favelas] = await executeQuery(
             `
             SELECT Nome_Fav,Nome_UPP, Qtd_de_familias, Grau_de_urbanizacao, Nome_Bairro, Nome_Comp
             FROM Favela
             NATURAL JOIN Bairro
-            NATURAL JOIN Complexo
+            NATURAL LEFT JOIN Complexo
             WHERE Cod_Fav=?
         `,
             [fid]
